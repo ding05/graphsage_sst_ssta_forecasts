101cb0e21db8465a0003b135eefbc094614fd4f5
Ding NING <ning.di@northeastern.edu>
2023-01-23 20:27:56 +1300

    Add files via upload

--- processing.py
+++ processing.py
@@ -6,14 +6,14 @@ import math
 
 import xarray as xr
 
-data_path = "data/"
+data_path = 'data/'
 
 # Read the datasets.
 
-era5_19592022 = xr.open_dataset(data_path + "era5_sst_t2m_011959_112022.nc", decode_times=False)
-era5_19501978 = xr.open_dataset(data_path + "era5_sst_t2m_011950_121978.nc", decode_times=False)
-print("ERA5:", era5_19592022, era5_19501978)
-print("--------------------")
+era5_19592022 = xr.open_dataset(data_path + 'era5_sst_t2m_011959_112022.nc', decode_times=False)
+era5_19501978 = xr.open_dataset(data_path + 'era5_sst_t2m_011950_121978.nc', decode_times=False)
+print('ERA5:', era5_19592022, era5_19501978)
+print('--------------------')
 print()
 
 """
@@ -26,33 +26,33 @@ era5_19501978 = era5_19501978.to_array()
 era5_19592022 -= 273.15
 era5_19501978 -= 273.15
 
-print("ERA5:", era5_19592022, era5_19501978)
-print("ERA5's shape:", era5_19592022.shape, era5_19501978.shape)
-print("--------------------")
+print('ERA5:', era5_19592022, era5_19501978)
+print('ERA5's shape:', era5_19592022.shape, era5_19501978.shape)
+print('--------------------')
 print()
 
 era5_sst_19592022 = np.array(era5_19592022[1,:-2,0,:,:]) # Fields at the last two time steps.
 era5_sst_19501978 = np.array(era5_19501978[1,:,:,:])
-print("ERA5 SST:", era5_sst_19592022, era5_sst_19501978)
-print("ERA5 SST's shape:", era5_sst_19592022.shape, era5_sst_19501978.shape)
-print("--------------------")
+print('ERA5 SST:', era5_sst_19592022, era5_sst_19501978)
+print('ERA5 SST's shape:', era5_sst_19592022.shape, era5_sst_19501978.shape)
+print('--------------------')
 print()
 
 era5_t2m_19592022 = np.array(era5_19592022[0,:-2,0,:,:])
 era5_t2m_19501978 = np.array(era5_19501978[0,:,:,:])
-print("ERA5 T2M:", era5_t2m_19592022, era5_t2m_19501978)
-print("ERA5 T2M's shape:", era5_t2m_19592022.shape, era5_t2m_19501978.shape)
-print("--------------------")
+print('ERA5 T2M:', era5_t2m_19592022, era5_t2m_19501978)
+print('ERA5 T2M's shape:', era5_t2m_19592022.shape, era5_t2m_19501978.shape)
+print('--------------------')
 print()
 
 era5_sst = np.concatenate((era5_sst_19501978, era5_sst_19592022[240:]))
 era5_t2m = np.concatenate((era5_t2m_19501978, era5_t2m_19592022[240:]))
 
-save(data_path + "era5_sst_grid.npy", era5_sst)
-save(data_path + "era5_t2m_grid.npy", era5_t2m)
+save(data_path + 'era5_sst_grid.npy', era5_sst)
+save(data_path + 'era5_t2m_grid.npy', era5_t2m)
 
-print("Save the two tensors in NPY files.")
-print("--------------------")
+print('Save the two tensors in NPY files.')
+print('--------------------')
 print()
 
 # Calculate SSTAs and T2MAs.
@@ -62,9 +62,9 @@ num_year = era5_sst.shape[0] / 12
 train_num_year = math.ceil(num_year * train_split)
 test_num_year = int(num_year - train_num_year)
 
-print("The number of years for training:", train_num_year)
-print("The number of years for testing:", test_num_year)
-print("----------")
+print('The number of years for training:', train_num_year)
+print('The number of years for testing:', test_num_year)
+print('----------')
 print()
 
 era5_ssta = []
@@ -77,16 +77,16 @@ for row in era5_t2m:
   era5_t2ma.append(get_ssta(row, train_num_year))
 era5_t2ma = np.array(era5_t2ma)
 
-print("ERA5 SSTA:", era5_ssta)
-print("ERA5 SSTA's shape:", era5_ssta.shape)
-print("ERA5 T2MA:", era5_t2ma)
-print("ERA5 T2MA's shape:", era5_t2ma.shape)
+print('ERA5 SSTA:', era5_ssta)
+print('ERA5 SSTA's shape:', era5_ssta.shape)
+print('ERA5 T2MA:', era5_t2ma)
+print('ERA5 T2MA's shape:', era5_t2ma.shape)
 
-save(data_path + "era5_ssta_grid.npy", era5_ssta)
-save(data_path + "era5_t2ma_grid.npy", era5_t2ma)
+save(data_path + 'era5_ssta_grid.npy', era5_ssta)
+save(data_path + 'era5_t2ma_grid.npy', era5_t2ma)
 
-print("Save the two tensors in NPY files.")
-print("--------------------")
+print('Save the two tensors in NPY files.')
+print('--------------------')
 print()
 
 # Combine the two grids into one grid, explained on Page 17, Taylor & Feng, 2022.
@@ -95,13 +95,13 @@ era5_combined = era5_sst
 mask = np.isnan(era5_sst) & ~np.isnan(era5_t2m)
 era5_combined[mask] = era5_t2m[mask]
 
-save(data_path + "era5_sst_t2m_grid.npy", era5_combined)
+save(data_path + 'era5_sst_t2m_grid.npy', era5_combined)
 
-print("ERA5 SSTA-T2M:", era5_combined)
-print("ERA5 SSTA-T2M's shape:", era5_combined.shape)
+print('ERA5 SSTA-T2M:', era5_combined)
+print('ERA5 SSTA-T2M's shape:', era5_combined.shape)
 
-print("Save the tensor in an NPY file.")
-print("--------------------")
+print('Save the tensor in an NPY file.')
+print('--------------------')
 print()
 """
 
@@ -113,9 +113,9 @@ sst_t2m_19592022 = era5_19592022.fillna(era5_19592022['t2m'])
 #print(sst_t2m_19592022.count())
 sst_t2m_19501978 = era5_19501978.fillna(era5_19501978['t2m'])
 
-sst_t2m_19592022.to_netcdf(path=data_path + "era5_sstwt2m_011959_112022.nc")
-sst_t2m_19501978.to_netcdf(path=data_path + "era5_sstwt2m_011950_121978.nc")
+sst_t2m_19592022.to_netcdf(path=data_path + 'era5_sstwt2m_011959_112022.nc')
+sst_t2m_19501978.to_netcdf(path=data_path + 'era5_sstwt2m_011950_121978.nc')
 
-print("Save the netCDF datasets in NC files.")
-print("--------------------")
+print('Save the netCDF datasets in NC files.')
+print('--------------------')
 print()
--- train_gcn.py
+++ train_gcn.py
@@ -3,6 +3,7 @@ from numpy import asarray, save, load
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.optim import Adam
 from torch_geometric.data import Data
 from torch_geometric.nn import GCNConv
@@ -16,34 +17,34 @@ out_path = 'out/'
 window_size = 12
 lead_time = 1
 learning_rate = 0.01
-num_epochs = 50
+num_epochs = 30
 
 # Load the data.
 
-node_feats_grid = load("data/node_feats.npy")
-print("Node feature grid in Kelvin:", node_feats_grid)
-print("Shape:", node_feats_grid.shape)
-print("----------")
+node_feats_grid = load('data/node_feats.npy')
+print('Node feature grid in Kelvin:', node_feats_grid)
+print('Shape:', node_feats_grid.shape)
+print('----------')
 print()
 
 # Convert Kelvin to Celsius.
 node_feats_grid -= 273.15
-print("Node feature grid in Celsius:", node_feats_grid)
-print("Shape:", node_feats_grid.shape)
-print("----------")
+print('Node feature grid in Celsius:', node_feats_grid)
+print('Shape:', node_feats_grid.shape)
+print('----------')
 print()
 
 # Normalize the data to [-1, 1].
 node_feats_grid_normalized = (node_feats_grid - np.min(node_feats_grid)) / (np.max(node_feats_grid) - np.min(node_feats_grid)) * 2 - 1
-print("Normalized node feature grid:", node_feats_grid_normalized)
-print("Shape:", node_feats_grid_normalized.shape)
-print("----------")
+print('Normalized node feature grid:', node_feats_grid_normalized)
+print('Shape:', node_feats_grid_normalized.shape)
+print('----------')
 print()
 
-adj_mat = load("data/adj_mat_0.9.npy")
-print("Adjacency matrix:", adj_mat)
-print("Shape:", adj_mat.shape)
-print("----------")
+adj_mat = load('data/adj_mat_0.95.npy')
+print('Adjacency matrix:', adj_mat)
+print('Shape:', adj_mat.shape)
+print('----------')
 print()
 
 # Compute the total number of time steps.
@@ -60,23 +61,24 @@ for time_i in range(num_time):
         x.append(node_feats_grid_normalized[node_i][time_i : time_i + window_size])
         # The outputs are node features in Celsius.
         y.append(node_feats_grid[node_i][time_i + window_size + lead_time - 1])
-        """
+        '''
         # The outputs are normalized node features.
         y.append(node_feats_grid_normalized[node_i][time_i + window_size + lead_time - 1])
-        """
+        '''
     x = torch.tensor(x)
     edge_index = torch.tensor(adj_mat, dtype=torch.long)
     data = Data(x=x, y=y, edge_index=edge_index, num_nodes=node_feats_grid.shape[0], num_edges=adj_mat.shape[1], has_isolated_nodes=True, has_self_loops=False, is_undirected=True)
+    #data = Data(x=x, y=y, num_nodes=node_feats_grid.shape[0], num_edges=adj_mat.shape[1], has_isolated_nodes=True, has_self_loops=False, is_undirected=True)
     graph_list.append(data)
 
 # Set the number of decimals in torch tensors printed.
 torch.set_printoptions(precision=8)
 
-print("Inputs of the first node in the first graph, i.e. the first time step:", graph_list[0].x[0])
-#print("Output of the first node in the first graph:", graph_list[0].y[0])
-print("Check if they match those in the node features:", node_feats_grid[0][:13])
-print("Check if they match those in the normalized node features:", node_feats_grid_normalized[0][:13])
-print("----------")
+print('Inputs of the first node in the first graph, i.e. the first time step:', graph_list[0].x[0])
+#print('Output of the first node in the first graph:', graph_list[0].y[0])
+print('Check if they match those in the node features:', node_feats_grid[0][:13])
+print('Check if they match those in the normalized node features:', node_feats_grid_normalized[0][:13])
+print('----------')
 print()
 
 # Split the data, following Taylor & Feng, 2022.
@@ -90,7 +92,8 @@ test_graph_list = graph_list[760:]
 class MultiGraphGCN(torch.nn.Module):
     def __init__(self, in_channels, hid_channels, out_channels, num_graphs):
         super(MultiGraphGCN, self).__init__()
-        self.convs = torch.nn.ModuleList([torch.nn.Sequential(GCNConv(in_channels, hid_channels), GCNConv(hid_channels, hid_channels), GCNConv(hid_channels, out_channels)) for _ in range(num_graphs)])
+        #self.convs = torch.nn.ModuleList([torch.nn.Sequential(GCNConv(in_channels, hid_channels), GCNConv(hid_channels, hid_channels), GCNConv(hid_channels, out_channels)) for _ in range(num_graphs)])
+        self.convs = torch.nn.ModuleList([torch.nn.Sequential(GCNConv(in_channels, hid_channels), GCNConv(hid_channels, hid_channels), GCNConv(hid_channels, hid_channels), GCNConv(hid_channels, out_channels)) for _ in range(num_graphs)])
         self.double()
     def forward(self, data_list):
         x_list = []
@@ -98,6 +101,9 @@ class MultiGraphGCN(torch.nn.Module):
             x = data.x
             for j, layer in enumerate(self.convs[i]):
                 x = layer(x, data.edge_index)
+                #x = F.tanh(x) # tanh() and sigmoid() suit y in [-1, 1].
+                #x = F.relu(x) # relu() ant its variant suit y in a larger range.
+                x = F.elu(x)
             x_list.append(x)
         x_concat = torch.cat(x_list, dim=0)
         return x_concat
@@ -114,8 +120,8 @@ optimizer = torch.optim.RMSprop(model.parameters(), lr=learning_rate, alpha=0.9,
 
 # Train a multi-graph GCN model, using and modifying the code generated by ChatGPT-3.
 
-print("Start training.")
-print("----------")
+print('Start training.')
+print('----------')
 print()
 
 # Start time
@@ -141,27 +147,27 @@ for epoch in range(num_epochs):
         for data in val_graph_list:
             output = model([data])
             val_mse = criterion(output.squeeze(), torch.tensor(data.y).squeeze())
-            print("Val predictions:", [round(i, 4) for i in output.squeeze().tolist()[::300]])
-            print("Val observations:", [round(i, 4) for i in torch.tensor(data.y).squeeze().tolist()[::300]])
+            print('Val predictions:', [round(i, 4) for i in output.squeeze().tolist()[::300]])
+            print('Val observations:', [round(i, 4) for i in torch.tensor(data.y).squeeze().tolist()[::300]])
             val_mse_nodes += val_mse
         val_mse_nodes /= len(val_graph_list)
         val_mse_nodes_epochs.append(round(val_mse_nodes.item(), 4))
-    print("----------")
+    print('----------')
     print()
 
     # Print the current epoch and validation MSE.
-    print("Epoch [{}/{}], Loss: {:.4f}, Validation MSE: {:.4f}".format(epoch + 1, num_epochs, loss.item(), val_mse_nodes))
-    print("Loss by epoch:", loss_epochs)
-    print("Validation MSE by epoch:", val_mse_nodes_epochs)
+    print('Epoch [{}/{}], Loss: {:.4f}, Validation MSE: {:.4f}'.format(epoch + 1, num_epochs, loss.item(), val_mse_nodes))
+    print('Loss by epoch:', loss_epochs)
+    print('Validation MSE by epoch:', val_mse_nodes_epochs)
 
-print("----------")
+print('----------')
 print()
 
 # End time
 stop = time.time()
 
-print(f"Complete training. Time spent: {stop - start} seconds.")
-print("----------")
+print(f'Complete training. Time spent: {stop - start} seconds.')
+print('----------')
 print()
 
 """
@@ -171,24 +177,24 @@ with torch.no_grad():
     for data in test_graph_list:
         output = model([data])
         test_mse = criterion(output.squeeze(), torch.tensor(data.y).squeeze())
-        print("Test predictions:", [round(i, 4) for i in output.squeeze().tolist()[::300]])
-        print("Test observations:", [round(i, 4) for i in torch.tensor(data.y).squeeze().tolist()[::300]])
+        print('Test predictions:', [round(i, 4) for i in output.squeeze().tolist()[::300]])
+        print('Test observations:', [round(i, 4) for i in torch.tensor(data.y).squeeze().tolist()[::300]])
         test_mse_nodes += test_mse
     test_mse_nodes /= len(test_graph_list)
-    print("Test MSE: {:.4f}".format(test_mse_nodes))
+    print('Test MSE: {:.4f}'.format(test_mse_nodes))
 
-print("----------")
+print('----------')
 print()
 """
 
 # Save the model.
 torch.save({
-            "epoch": num_epochs,
-            "model_state_dict": model.state_dict(),
-            "optimizer_state_dict": optimizer.state_dict(),
-            "loss": loss
-            }, models_path + "_GCN_" + str(stop))
+            'epoch': num_epochs,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': loss
+            }, models_path + '_GCN_' + str(stop))
 
-print("Save the checkpoint in a TAR file.")
-print("----------")
+print('Save the checkpoint in a TAR file.')
+print('----------')
 print()
