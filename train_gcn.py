cbeaf61aee2a9f5eba51e77d010b0a6ad31dd58e
Ding NING <ning.di@northeastern.edu>
2023-03-29 04:33:55 +1300

    Add files via upload

--- train_gcn.py
+++ train_gcn.py
@@ -15,7 +15,7 @@ models_path = 'configs/'
 out_path = 'out/'
 
 node_filename = 'node_feats.npy'
-adj_filename = 'adj_mat_0.5.npy'
+adj_filename = 'adj_mat_0.3.npy'
 #adj_filename = 'adj_mat_0.9_directed.npy'
 
 window_size = 12
