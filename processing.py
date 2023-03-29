83cd33342981227efa11fb25ba9b0fedcbd4002f
Ding NING <ning.di@northeastern.edu>
2023-03-29 16:05:36 +1300

    Add files via upload

--- processing.py
+++ processing.py
@@ -8,17 +8,15 @@ import xarray as xr
 
 data_path = 'data/'
 
+"""
 # Read the datasets.
-
 era5_19592022 = xr.open_dataset(data_path + 'era5_sst_t2m_011959_112022.nc', decode_times=False)
 era5_19501978 = xr.open_dataset(data_path + 'era5_sst_t2m_011950_121978.nc', decode_times=False)
 print('ERA5:', era5_19592022, era5_19501978)
 print('--------------------')
 print()
 
-"""
 # Turn datasets into arrays and get wanted variables.
-
 era5_19592022 = era5_19592022.to_array()
 era5_19501978 = era5_19501978.to_array()
 
@@ -56,7 +54,6 @@ print('--------------------')
 print()
 
 # Calculate SSTAs and T2MAs.
-
 train_split = 0.8
 num_year = era5_sst.shape[0] / 12
 train_num_year = math.ceil(num_year * train_split)
@@ -90,7 +87,6 @@ print('--------------------')
 print()
 
 # Combine the two grids into one grid, explained on Page 17, Taylor & Feng, 2022.
-
 era5_combined = era5_sst
 mask = np.isnan(era5_sst) & ~np.isnan(era5_t2m)
 era5_combined[mask] = era5_t2m[mask]
@@ -103,18 +99,62 @@ print('ERA5 SSTA-T2M's shape:', era5_combined.shape)
 print('Save the tensor in an NPY file.')
 print('--------------------')
 print()
-"""
 
 # Else, do it on NC files directly.
 # Count the number of non-NA values.
-#print(era5_19592022.count())
 sst_t2m_19592022 = era5_19592022.fillna(era5_19592022['t2m'])
-#print(sst_t2m_19592022)
-#print(sst_t2m_19592022.count())
 sst_t2m_19501978 = era5_19501978.fillna(era5_19501978['t2m'])
 
 sst_t2m_19592022.to_netcdf(path=data_path + 'era5_sstwt2m_011959_112022.nc')
 sst_t2m_19501978.to_netcdf(path=data_path + 'era5_sstwt2m_011950_121978.nc')
+
+print('Save the netCDF datasets in NC files.')
+print('--------------------')
+print()
+
+"""
+
+# Process SST grids into SSTA grids.
+
+train_num_year = 840
+
+# Read the dataset.
+era5_19402022_globe = xr.open_dataset(data_path + 'era5_sst_011940_122022_globe.nc', decode_times=False)
+print('ERA5 Globe:', era5_19402022_globe)
+print('--------------------')
+print()
+
+# Convert SSTs into SSTAs.
+sst_19402022 = era5_19402022_globe['sst']
+sst_19402022_np = sst_19402022.values
+
+print(sst_19402022_np)
+print("ERA5 Globe's shape:", sst_19402022_np.shape)
+print('--------------------')
+print()
+
+ssta_19402022_np = np.empty(sst_19402022_np.shape)
+
+for lat_idx in range(sst_19402022_np.shape[1]):
+    for lon_idx in range(sst_19402022_np.shape[2]):
+        time_series = sst_19402022_np[:, lat_idx, lon_idx]
+        
+        if np.all(np.isnan(time_series)):
+            ssta_19402022_np[:, lat_idx, lon_idx] = np.nan
+            continue
+        
+        new_time_series = get_ssta(time_series, train_num_year)
+        ssta_19402022_np[:, lat_idx, lon_idx] = new_time_series
+
+print(ssta_19402022_np)
+print("ERA5 SSTA Globe's shape:", ssta_19402022_np.shape)
+print('--------------------')
+print()
+
+# Write the new dataset.
+era5_19402022_ssta_globe = era5_19402022_globe
+era5_19402022_ssta_globe['sst'].values = ssta_19402022_np
+era5_19402022_ssta_globe.to_netcdf(data_path + 'era5_ssta_011940_122022_globe.nc')
 
 print('Save the netCDF datasets in NC files.')
 print('--------------------')
